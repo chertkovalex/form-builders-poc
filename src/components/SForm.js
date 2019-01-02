31d873735cda85178fc81258430c795ca542e61a
Alexander Chertkov <chertkovalex@gmail.com>
2019-01-02 17:11:43 +0200

    Change schema

--- src/components/SForm.js
+++ src/components/SForm.js
@@ -2,20 +2,89 @@ import React from 'react';
 
 import Form from 'react-jsonschema-form';
 
-const schema = {
-  title: 'Todo',
+const schema2 = {
+  title: 'A registration form',
+  description: 'A simple form example.',
   type: 'object',
-  required: ['title'],
+  required: ['firstName', 'lastName'],
   properties: {
-    title: { type: 'string', title: 'Title', default: 'A new task' },
-    done: { type: 'boolean', title: 'Done?', default: false }
+    firstName: {
+      type: 'string',
+      title: 'First name'
+    },
+    lastName: {
+      type: 'string',
+      title: 'Last name'
+    },
+    age: {
+      type: 'integer',
+      title: 'Age'
+    },
+    bio: {
+      type: 'string',
+      title: 'Bio'
+    },
+    password: {
+      type: 'string',
+      title: 'Password',
+      minLength: 3
+    },
+    telephone: {
+      type: 'string',
+      title: 'Telephone',
+      minLength: 10
+    }
   }
+};
+
+const uiSchema = {
+  firstName: {
+    'ui:autofocus': true,
+    'ui:emptyValue': ''
+  },
+  age: {
+    'ui:widget': 'updown',
+    'ui:title': 'Age of person',
+    'ui:description': '(earthian year)'
+  },
+  bio: {
+    'ui:widget': 'textarea'
+  },
+  password: {
+    'ui:widget': 'password',
+    'ui:help': 'Hint: Make it strong!'
+  },
+  date: {
+    'ui:widget': 'alt-datetime'
+  },
+  telephone: {
+    'ui:options': {
+      inputType: 'tel'
+    }
+  }
+};
+
+const formData = {
+  firstName: 'Chuck',
+  lastName: 'Norris',
+  age: 75,
+  bio: 'Roundhouse kicking asses since 1940',
+  password: 'noneed'
 };
 
 const log = type => console.log.bind(console, type);
 
 const SForm = () => {
-  return <Form schema={schema} onChange={log('changed')} onSubmit={log('submitted')} onError={log('errors')} />;
+  return (
+    <Form
+      schema={schema2}
+      uiSchema={uiSchema}
+      formData={formData}
+      onChange={log('changed')}
+      onSubmit={log('submitted')}
+      onError={log('errors')}
+    />
+  );
 };
 
 export default SForm;
