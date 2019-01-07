a846700fb0273b8fb6822d3a738b31bb434fdfd5
Alexander Chertkov <chertkovalex@gmail.com>
2019-01-07 12:22:40 +0200

    Added simple form, but it doesn't work

--- src/components/SForm.js
+++ src/components/SForm.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import * as MUI from 'material-ui';
+import { Aztec } from 'react-aztec';
 
-const SForm = () => {
-  return <form>Form</form>;
-};
+const formData = [
+  {
+    id: "name",
+    type: 'textfield',
+    props: {
+      id: 'name',
+      floatingLabelText: 'Hello, Whats your name?',
+      hintText: 'Name is required'
+    },
+    rules: {
+      validation: [
+        {
+          rule: 'mandatory', //email, lowercase, mobile
+          message: 'Name is required!!' // on error message to be displayed
+        }
+      ]
+    }
+  }
+];
 
+class SForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <Aztec
+          guid="unique-id"
+          data={formData}
+          library={MUI}
+        />
+      </div>
+    )
+  }
+}
 export default SForm;
